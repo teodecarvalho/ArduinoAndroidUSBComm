7888d564a6e38b90b6c79f5186df9b913605aba9
Teo <teo.decarvalho@gmail.com>
2020-12-11 17:00:14 -0300

    Added the run_safely decorator to improve code readability

--- Utilities.py
+++ Utilities.py
@@ -1,9 +1,22 @@
-from time import sleep
+from functools import wraps
 
-def try_n_times(func, max_tries = 10, sleep_time = 1, **kwargs):
-    for i in range(0, max_tries):
-        try:
-            return func(**kwargs)
-        except:
-            sleep(sleep_time)
-    raise Exception("Something went wrong with the function '" + func.__name__ + "'!")
+def run_safely(func, max_tries = 10, sleep_time = .5):
+    from time import sleep
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        for i in range(0, max_tries):
+            try:
+                return func(*args, **kwargs)
+            except:
+                sleep(sleep_time)
+        raise Exception("Something went wrong with the function '" + func.__name__ + "'!")
+    return wrapper
+
+def debug(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"***Function {func.__name__} was called with the following arguments: {args}, and {kwargs}.")
+        result = func(*args, **kwargs)
+        print(f"***Function {func.__name__} returned: {result}")
+        return result
+    return wrapper
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.boxlayout import BoxLayout
 from USBSerial import USBdevice
 from time import sleep
 from kivy.clock import Clock
-from Utilities import try_n_times
+from Utilities import run_safely
 
 class LedWidget(BoxLayout):
     def __init__(self, **kwargs):
@@ -11,41 +11,39 @@ class LedWidget(BoxLayout):
         self.USBdevice = USBdevice()
 
     def connect_usb(self):
-        self.USBdevice.safe_connect_usb()
+        self.USBdevice.connect_usb()
 
     def flush(self):
         self.USBdevice.flush()
 
     def send_cmd(self, cmd):
-        self.USBdevice.safe_write_to_device(cmd = cmd)
+        self.USBdevice.write_to_device(cmd = cmd)
     
     def rec_msg(self):
-        msg = self.USBdevice.safe_read_from_device()
+        msg = self.USBdevice.read_from_device()
         return msg
 
     def turn_led_on(self):
-        self.send_cmd("<on>")
+        self.send_cmd(cmd = "<on>")
 
     def turn_led_off(self):
-        self.send_cmd("<of>")
+        self.send_cmd(cmd = "<of>")
     
     def parse_led_state(self, state):
         return int(state.splitlines()[1])
-        
+
+    @run_safely
     def get_led_state(self):
         self.flush()
         sleep(.1)
-        self.send_cmd("<ls>")
+        self.send_cmd(cmd = "<ls>")
         sleep(.1)
         state = self.rec_msg()
         print(state)
         return self.parse_led_state(state)
 
-    def safe_get_led_state(self):
-        return try_n_times(self.get_led_state)
-
     def update_led_label(self, dt):
-        state = self.safe_get_led_state()
+        state = self.get_led_state()
         if state:
             self.ids.led_state_label.text = "On"
         else:
